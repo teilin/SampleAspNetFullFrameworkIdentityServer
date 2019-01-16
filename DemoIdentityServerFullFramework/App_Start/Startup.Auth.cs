e79ade08a0b626a0cc7bc920cca9238d9484f92a
Teis Lindemark <teis@acos.no>
2019-01-16 12:30:04 -0800

    Init commit

---
+++ DemoIdentityServerFullFramework/App_Start/Startup.Auth.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using DemoIdentityServerFullFramework.Models;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace DemoIdentityServerFullFramework
+{
+    public partial class Startup {
+
+        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301883
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                ClientId = "ip.client",
+                ClientSecret = "",
+                Authority = "",
+                RedirectUri = "https://localhost:44301/",
+                ResponseType = "id_token",
+                Scope = "openid profile",
+
+                UseTokenLifetime = false,
+                SignInAsAuthenticationType = "Cookies"
+            });
+        }
+    }
+}
